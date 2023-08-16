9e9ff74190815f94caaae0849ea97840600772a1
Денис <tequila96@bk.ru>
2023-08-16 12:09:43 +0500

    Сделал ДЗ

---
+++ src/Book.java
@@ -0,0 +1,26 @@
+public class Book {
+    private String title;
+    private Author author;
+    private int publicationYear;
+
+    public Book(String title, Author author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
